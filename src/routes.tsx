16288c8ef261f495ea1287376603fc057f53a78f
thaihdph27014 <hoangducthai2301@gmail.com>
2023-10-04 13:07:15 +0700

    new

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,27 +1,22 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
-
-
-
-
-import DetailPage from "./pages/Client/detail";
-
-import BookTour from "./pages/Client/Book_tour";
-
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/homepage";
+// import TourPage from "./pages/TourPage";
+// import NewPage from "./pages/NewPage";
+// import TitelPage from "./pages/TitelPage";
+import Signup from "./auth/signup";
+import SignIn from "./auth/signin";
 export const router = createBrowserRouter([
   {
-    path: "/:id/tour",
-    element: <DetailPage/>,
+    path: "/",
+    element: <div><HomePage /></div>,
   },
-  {
-    path: "/booktour",
-    element: <BookTour/>,
-  },
-
-  
-
-
-    
+// ,
+//     {path: "/tour",element:<TourPage/>},
+//     {path: "/New",element:<NewPage/>},
+//     {path: "/title",element:<TitelPage/>},
+//     {path: "/tour",element:<TourPage/>},
+    {path: "/signin",element:<SignIn/>},
+    {path: "/signup",element:<Signup/>},
   
   { path: "*", element: "Not Found Page" },
 ]);
-
